23fa4731ee021f5deab5f8ee82455604a0202efa
Trinh Vo Duc Tai <taitvd250803@gmail.com>
2023-11-29 10:19:28 +0700

    Update Full Source

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Frontend-Framework FPOLY using Bootstrap v5.3.0, AngularJS v1.8.2, Angular Route
+# Frontend-Frameworks FPOLY using Bootstrap v5.3.0, AngularJS v1.8.2, Angular Route
+Donate? Muốn hỗ trợ mình 1 ly cafe <3 [Donate here](https://github.com/taitvd2508/taitvd2508)
+# Cơ bản
 Trang chủ
 ![Trang chủ](./review/index.png)
+
+
+Đăng nhập
+Username: trinhtai25
+Password: 123456
+![Đăng nhập](./review/login.png)
+
+Đăng ký
+![Đăng ký](./review/register.png)
--- js/script.js
+++ js/script.js
@@ -39,37 +39,36 @@ app.controller("myCtrl", function($scope, $http, $rootScope, $location){
 
     // đăng ký
     $scope.dangky = function(){
-        for(var i=0; i < listsv.length; i++){
-         if($scope.student.username == listsv[i].username) {
-             alert("Trùng Username ! Vui lòng nhập Username khác");
-             return false;
-         }else if($scope.student.password2 != $scope.student.password){
-             alert("Vui Lòng Nhập Đúng Mật Khẩu Xác Nhận !");
-             return false;
-         }else{
-             listsv.push(angular.copy($scope.student));
-             alert("Đăng ký thành công !");
-             $location.path('/dangnhap');
-             console.log(listsv);
-             return true;
-         }
-     }
-     }
- 
-     $scope.dangxuat = function(){
-         $rootScope.dangnhapchua = "";
-         alert("Đã Đăng Xuất !");
-         $location.path('/dangnhap');
-         $rootScope.hoten = "";
-         $rootScope.email = "";
-     }
- 
-     $rootScope.huy = function() {
-         $location.path('/trangchu');
-     }
+       for(var i=0; i < listsv.length; i++){
+        if($scope.student.username == listsv[i].username) {
+            alert("Trùng Username ! Vui lòng nhập Username khác");
+            return false;
+        }else if($scope.student.password2 != $scope.student.password){
+            alert("Vui Lòng Nhập Đúng Mật Khẩu Xác Nhận !");
+            return false;
+        }else{
+            listsv.push(angular.copy($scope.student));
+            alert("Đăng ký thành công !");
+            $location.path('/dangnhap');
+            console.log(listsv);
+            return true;
+        }
+    }
+    }
+
+    $scope.dangxuat = function(){
+        $rootScope.dangnhapchua = "";
+        alert("Đã Đăng Xuất !");
+        $location.path('/dangnhap');
+        $rootScope.hoten = "";
+        $rootScope.email = "";
+    }
+
+    $rootScope.huy = function() {
+        $location.path('/trangchu');
+    }
 });
 
-// ĐĂNG NHẬP
 app.controller("dangnhapCtrl", function ($scope, $rootScope, $location) {
     $scope.dangnhap = function () {
         var user = $scope.user;
@@ -93,6 +92,126 @@ app.controller("dangnhapCtrl", function ($scope, $rootScope, $location) {
             alert("Đăng nhập thất bại ! Vui lòng nhập đúng Username & Password");
         }
     }
+});
+
+// ĐỔI MẬT KHẨU - CONTROLLER
+app.controller("doimatkhauCtrl", function ($scope, $rootScope, $location) {
+    if (!$rootScope.dangnhapchua) {
+        alert("Bạn chưa đăng nhập. Vui lòng đăng nhập !");
+        $location.path('/dangnhap');
+    }
+
+    // HÀM ĐỔI MẬT KHẨU
+    $scope.doimatkhau = function () {
+        for (var i = 0; i < listsv.length; i++) {
+            if ($rootScope.sinhvien_login.username == listsv[i].username) {
+                if ($rootScope.sinhvien_login.password != $scope.student.password) { // $rootScope.sinhvien_login (lấy giá trị của sinh viên sau khi đăng nhập thành công)
+                    alert("Mật Khẩu Hiện Tại Không Đúng !");
+                    return false;
+                } else if ($rootScope.sinhvien_login.password == $scope.student.password_new) {
+                    alert("Mật Khẩu Mới Không Được Trùng Với Mật Khẩu Hiện Tại !");
+                    return false;
+                } else if ($scope.student.password_new != $scope.student.password_new2) {
+                    alert("Vui Lòng Nhập Mật Khẩu Xác Nhận Đúng Với Mật Khẩu Mới !");
+                    return false;
+                } else {
+                    $rootScope.sinhvien_login.password = angular.copy($scope.student.password_new);
+                    alert("Đổi mật khẩu thành công !")
+                    $location.path('/trangchu');
+                    console.log(listsv);
+                    return true;
+                }
+            }
+        }
+    }
+});
+
+app.controller("capnhatCtrl", function ($scope, $rootScope, $location) {
+    if (!$rootScope.dangnhapchua) {
+        alert("Bạn chưa đăng nhập. Vui lòng đăng nhập !");
+        $location.path('/dangnhap');
+    }
+
+    $scope.edit = function() {
+        $scope.student = angular.copy($rootScope.sinhvien_login); //copy sinh viên login ở trên vào $scope.student
+    };
+
+    $scope.capnhat = function () {
+        for (var i = 0; i < listsv.length; i++) {
+            if ($rootScope.sinhvien_login.username == listsv[i].username) {
+                $rootScope.sinhvien_login.fullname = angular.copy($scope.student.fullname); // đổi thông tin
+                $rootScope.sinhvien_login.email = angular.copy($scope.student.email);
+                $rootScope.sinhvien_login.birthday = angular.copy($scope.student.birthday);
+                $rootScope.sinhvien_login.schoolfee = angular.copy($scope.student.schoolfee);
+                $rootScope.sinhvien_login.marks = angular.copy($scope.student.marks);
+                $rootScope.sinhvien_login.gender = angular.copy($scope.student.gender);
+                alert("Đổi thông tin tài khoản thành công !")
+                $rootScope.hoten = $rootScope.sinhvien_login.fullname; // gán hoten = hoten mới cập nhật
+                $rootScope.email = $rootScope.sinhvien_login.email;
+                $location.path('/trangchu');
+                console.log(listsv);
+                return true;
+            }
+        }
+    }
+});
+
+app.controller("tnCtrl", function($scope, $http, $routeParams, $interval, $location, $rootScope){
+    if(!$rootScope.dangnhapchua){
+        alert("Bạn chưa đăng nhập. Vui lòng đăng nhập !");
+        $location.path('/dangnhap');
+    }
+
+    
+    $scope.caccauhoi = [];
+    $scope.st = 1;
+    $scope.pageSize = 1;
+    $scope.start = 0;
+    $scope.tenMH = $routeParams.tenMH;
+    $scope.idMH = $routeParams.idMH;
+
+    $http.get("db/Quizs/" +$scope.idMH+ ".js").then(
+        function(t) {$scope.caccauhoi = t.data, $scope.tongst = Math.ceil($scope.caccauhoi.length / $scope.pageSize)},
+        function(f) {alert("Lỗi: " +f.statusText);}
+    );
+
+    $scope.next = function(){
+        if($scope.start < $scope.caccauhoi.length - $scope.pageSize){
+            $scope.start += $scope.pageSize;
+            $scope.st++;
+        }
+    }
+
+    $scope.prev = function(){
+        if($scope.start > 0){
+            $scope.start -= $scope.pageSize;
+            $scope.st--;
+        }
+    }
+
+    $scope.first = function(){
+        $scope.start = 0;
+        $scope.st = 1;
+    }
+
+    $scope.last = function(){
+        sotrang = Math.ceil($scope.caccauhoi.length / $scope.pageSize);
+        $scope.start = (sotrang - 1) * $scope.pageSize;
+        $scope.st = Math.ceil($scope.caccauhoi.length / $scope.pageSize);
+    }
+
+    $scope.time = 600;
+    $scope.demnguoc = function(){
+        if($scope.time > 0){
+            $scope.time--;
+            $scope.sophut = Math.floor($scope.time / 60); // làm tròn xuống 1 số
+            $scope.sogiay = $scope.time % 60;
+        }else{
+            return false;
+        }
+    }
+    $scope.demnguoc();
+    $interval($scope.demnguoc, 1000);
 });
 
 
